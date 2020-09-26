054449fbc5beb42a9eb64004c5638a5542e07ef2
Brandell Cassio <brandell.caassio@gmail.com>
2020-09-27 00:22:47 +0100

    Fixing experiments_query and Updating notebook

--- nalir/architecture/experiment_query.py
+++ nalir/architecture/experiment_query.py
@@ -5,22 +5,22 @@ import os
 import logging
 import xml.etree.ElementTree as et
 
-from rdbms.rdbms import RDBMS
-from data_structure.query import Query
-from components.stanford_parser import StanfordParser
-from components.node_mapper import NodeMapper
-from components.entity_resolution import entity_resolute
-from components.tree_structure_adjustor import TreeStructureAdjustor
-from components.sql_translator import translate
+from nalir.data_structure.query import Query
+from nalir.components.stanford_parser import StanfordParser
+from nalir.components.node_mapper import NodeMapper
+from nalir.components.entity_resolution import entity_resolute
+from nalir.components.tree_structure_adjustor import TreeStructureAdjustor
+from nalir.components.explainer import explain
+from nalir.components.sql_translator import translate
 
 
 def run_query(nl_query, rdbms, config):
     query = Query(nl_query,rdbms.schema_graph)
     
-    StanfordParser(query)
+    StanfordParser(query, config)
     NodeMapper.phrase_process(query,rdbms,config)
     entity_resolute(query)
     TreeStructureAdjustor.tree_structure_adjust(query,rdbms)
     translate(query, rdbms)
-    
+    explain(query)
     return query.translated_sql
